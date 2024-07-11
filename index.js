32f5123836a7b63e2b1cb96182905eba2ce5ced8
Gihan Chamila <algihanchamila@gmail.com>
2024-07-11 21:13:00 +0530

    create server

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import http from 'http'
 import app from './app.js'
 import { port } from "./config/kyes.js"
 
-// create server
+// Ceate server
 const server = http.createServer(app)
 
-// listen server
+// Listen server
 app.listen(port, () => console.log(`Server is running on port ${port}`))
