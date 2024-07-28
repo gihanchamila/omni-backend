0a3afa601c687b9cc8e0d4d61b54b76f8ef61a68
Gihan Chamila <algihanchamila@gmail.com>
2024-07-28 20:46:00 +0530

    add upload file method

--- controllers/file.js
+++ controllers/file.js
@@ -2,3 +2,39 @@ import path from "path"
 import { validateExtention } from "../validators/file.js"
 import { uploadFileToS3, signedUrl, deleteFilesFromS3 } from "../utils/awsS3.js"
 import File from "../models/File.js"
+
+const fileController = {
+    uploadFile : async (req, res, next) => {
+        try{
+
+            const {file} = req;
+            if(!file){
+                res.code = 400;
+                throw new Error("File not selected")
+            }
+
+            const ext = path.extname(file.originalname)
+            const isValidext = validateExtention(ext)
+
+            if(!isValidext){
+                res.code = 400;
+                throw new Error("Only 'jpg', 'jpeg' or 'png' is allowed")
+            }
+
+            const key = await uploadFileToS3({file, ext})
+            if(key){
+                const newFile = new File({
+                    key,
+                    size : file.size,
+                    mimetype : file.mimetype,
+                    createdBy : req.user._id
+                })
+            }
+
+        }catch(error){
+            next(error)
+        }
+    },
+
+    
+}
