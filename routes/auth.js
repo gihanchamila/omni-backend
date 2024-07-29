1d2a56425803f74d9adeee39b8eabaa557802da9
Gihan Chamila <algihanchamila@gmail.com>
2024-07-29 16:10:22 +0530

    add update profile route

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 import express from "express"
 import { authController} from "../controllers/index.js"
-import { signupValidator, signinValidator, verifyUserValidator, emailValidator, recoverPasswordValidator, changePasswordValidator } from "../validators/auth.js"
+import { signupValidator, signinValidator, verifyUserValidator, emailValidator, recoverPasswordValidator, changePasswordValidator, updateProfileValidator } from "../validators/auth.js"
 import { validate } from "../validators/validate.js"
+import isAuth from "../middlewares/isAuth.js"
 
 const router = express.Router()
 
@@ -11,7 +12,8 @@ router.post("/send-verification-email", emailValidator, validate, authController
 router.post("/verify-user", verifyUserValidator, validate, authController.verifyUser)
 router.post("/forgot-password-code", emailValidator, validate, authController.forgotPasswordCode)
 router.post("/recover-password", recoverPasswordValidator, validate, authController.recoverPassword)
-router.post("/change-password", changePasswordValidator, validate, authController.changePassword)
+router.post("/change-password",isAuth,  changePasswordValidator, validate, authController.changePassword)
+router.post("/update-profile",isAuth, updateProfileValidator, validate, authController.updateProfilePic)
 
 export default router
 
