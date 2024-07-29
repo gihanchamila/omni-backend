158680eb7cac7de57aaf5579825acf14f74cd4df
Gihan Chamila <algihanchamila@gmail.com>
2024-07-29 19:33:17 +0530

    replyToComment controller added

--- controllers/comment.js
+++ controllers/comment.js
@@ -23,6 +23,41 @@ const commentController = {
         } catch (error) {
             next(error)
         }
+    },
+
+    replyToComment : async (req, res, next) => {
+        try {
+            const { content } = req.body;
+            const { commentId, postId } = req.params;
+
+            // Find the parent comment
+            const parentComment = await Comment.findById(commentId).populate('user', 'name');
+            if (!parentComment) {
+                return res.status(404).json({ message: 'Parent comment not found' });
+            }
+
+            // Find the author of the parent comment
+            const parentAuthor = parentComment.user;
+            const mention = `@${parentAuthor.name}`;
+
+            // Create the reply
+            const reply = new Comment({
+                content: `${mention} ${content}`,
+                user: req.user._id,
+                post: postId,
+                parentComment: commentId,
+                mentions: [parentAuthor._id]
+            });
+
+            // Save the reply and update the parent comment
+            parentComment.replies.push(reply._id);
+            await parentComment.save();
+            await reply.save();
+
+            res.status(201).json({ code : 201, status : true,  message: 'Reply added', data : {reply} });
+        } catch (error) {
+            next(error)
+        }
     }
 
 }
--- models/Comment.js
+++ models/Comment.js
@@ -6,7 +6,8 @@ const commentSchema = mongoose.Schema({
     post: { type: mongoose.Types.ObjectId, ref: 'Post', required: true },
     parentComment: { type: mongoose.Types.ObjectId, ref: 'Comment', default: null },
     likes: [{ type: mongoose.Types.ObjectId, ref: 'Like' }],
-    replies: [{ type: mongoose.Types.ObjectId, ref: 'Comment' }]
+    replies: [{ type: mongoose.Types.ObjectId, ref: 'Comment' }],
+    mentions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 }, { timestamps: true });
 
 const Comment = mongoose.model("comment", commentSchema)
