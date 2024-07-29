24caa6b668b5d05692b9d24e7aaf93418b0888a6
Gihan Chamila <algihanchamila@gmail.com>
2024-07-29 16:07:09 +0530

    updateProfilePic controller add

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { authController} from "../controllers/index.js"
-import { signupValidator, signinValidator, verifyUserValidator, emailValidator, recoverPasswordValidator } from "../validators/auth.js"
+import { signupValidator, signinValidator, verifyUserValidator, emailValidator, recoverPasswordValidator, changePasswordValidator } from "../validators/auth.js"
 import { validate } from "../validators/validate.js"
 
 const router = express.Router()
@@ -11,6 +11,7 @@ router.post("/send-verification-email", emailValidator, validate, authController
 router.post("/verify-user", verifyUserValidator, validate, authController.verifyUser)
 router.post("/forgot-password-code", emailValidator, validate, authController.forgotPasswordCode)
 router.post("/recover-password", recoverPasswordValidator, validate, authController.recoverPassword)
+router.post("/change-password", changePasswordValidator, validate, authController.changePassword)
 
 export default router
 
