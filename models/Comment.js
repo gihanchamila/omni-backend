86a31d289d662b65fa360c61d55af5fbf6976db0
Gihan Chamila <algihanchamila@gmail.com>
2024-07-29 17:00:03 +0530

    create comment model

---
+++ models/Comment.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const commentSchema = new Schema({
+    content: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
+    parentComment: { type: Schema.Types.ObjectId, ref: 'Comment', default: null },
+    likes: [{ type: Schema.Types.ObjectId, ref: 'Like' }],
+    replies: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+}, { timestamps: true });
+
+const Comment = mongoose.model("comment", commentSchema)
+export default Comment
