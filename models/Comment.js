313563c19280e88243a55c1024e3b981e80d48b5
Gihan Chamila <algihanchamila@gmail.com>
2024-08-15 18:12:02 +0530

    comment model updated for reply comments

--- models/Comment.js
+++ models/Comment.js
@@ -4,7 +4,10 @@ import mongoose from "mongoose";
 const commentSchema = new mongoose.Schema({
     postId: { type: mongoose.Schema.Types.ObjectId, ref: 'post', required: true },
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
-    content: { type: String, required: true }
+    content: { type: String, required: true },
+    parentComment: { type: mongoose.Schema.Types.ObjectId, ref: 'comment', default: null }, // Added for replies
+    replies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'comment' }], // Added for replies
+    mentions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }]
 },{
     timestamps : true
 });
