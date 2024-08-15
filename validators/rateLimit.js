16de4e75c8e25ef415b49f45115605506a27b7fe
Gihan Chamila <algihanchamila@gmail.com>
2024-08-15 18:13:37 +0530

    rate limit for 15 minutes validator

---
+++ validators/rateLimit.js
@@ -0,0 +1,8 @@
+import rateLimit from 'express-rate-limit';
+
+// Create a rate limit rule
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again after 15 minutes"
+});
