586d83e7a5873411709db55cd19e93942e688a81
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-02-18 12:40:46 +0530

    add vercel for cors

--- utils/socket.js
+++ utils/socket.js
@@ -4,7 +4,7 @@ let io;
 export const initializeSocket = (server) => {
     io = new Server(server, {
         cors: {
-            origin: "http://localhost:5173",
+            origin: ["http://localhost:5173", "https://omni-frontend-eta.vercel.app"],
             methods: ["GET", "POST"]
         }
     });
