d3556b2a5374c96fe6c14edcb1ae70733b8bd4b2
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-02-18 21:29:42 +0530

    localhost add

--- app.js
+++ app.js
@@ -21,13 +21,7 @@ connectMongodb()
 const app = express()
 
 // third-party middleware
-app.use(
-    cors({
-      origin: ['http://localhost:5173'],
-      methods: ['GET', 'POST'],
-      credentials: true,
-    })
-  );
+app.use(cors({origin: "http://localhost:5173"}))
 app.use(express.json({limit : "500mb"}));
 app.use(bodyParser.urlencoded({limit : "500mb", extended : true}));
 app.use(morgan("dev"))
