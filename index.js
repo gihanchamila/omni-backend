6d0f4d3d6bcd6c43023e6bce402dc882d1b421ae
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-02-18 22:06:55 +0530

    update port

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ const server = http.createServer(app)
 initializeSocket(server);
 
 // Listen server
-server.listen(port, () => console.log(`Server is running on port ${port}`))
+server.listen(process.env.PORT || 8080, () => console.log(`Server is running on port ${port}`))
 
