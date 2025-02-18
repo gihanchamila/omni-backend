0725aec0207e9bc1d7b0a8860ab5b4780fd1c383
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-02-18 21:58:57 +0530

    update cors according to vercel domain

--- utils/socket.js
+++ utils/socket.js
@@ -4,7 +4,7 @@ let io;
 export const initializeSocket = (server) => {
     io = new Server(server, {
         cors: {
-            origin: "http://localhost:5173",
+            origin: ["https://omni-frontend-steel.vercel.app"], // Vercel URL
             methods: ["GET", "POST"]
         }
     });
